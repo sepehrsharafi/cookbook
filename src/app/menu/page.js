d286960d7b60d1f05bf9ca48a8007db70e48f369
sepehrsharafi <sepehr.sharafi123@gmail.com>
2025-04-12 02:52:00 +0330

    Task: Added backdrop blur to the header

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -5,6 +5,7 @@ export default async function Page() {
   return (
     <>
       <Header placeholder="برگشت به صفحه اصلی" route={"menu"} />
+
       <main className="flex flex-col gap-6 mb-5">
         <Suggestion
           id={1}
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -10,35 +10,38 @@ export default function Header({ placeholder, route }) {
   };
 
   return (
-    <header className="flex flex-row items-center h-[70px] border-b-[1px] border-[#E2E8F0] px-5 gap-[10px]">
-      <button
-        onClick={goToThePreviousPage}
-        className="p-2 rounded-[6px] border-[2px] border-[#E2E8F0]"
-      >
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+    <>
+      <header className="fixed top-0 w-full bg-white/93 backdrop-blur-[2px] flex flex-row items-center h-[70px] border-b-[1px] border-[#E2E8F0] px-5 gap-[10px]">
+        <button
+          onClick={goToThePreviousPage}
+          className="p-2 rounded-[6px] border-[2px] border-[#E2E8F0]"
         >
-          <path
-            d="M5 12H19"
-            stroke="black"
-            strokeWidth="2.35"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M12 5L19 12L12 19"
-            stroke="black"
-            strokeWidth="2.35"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </button>
-      <span className="text-xl font-[450]">{placeholder}</span>
-    </header>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5 12H19"
+              stroke="black"
+              strokeWidth="2.35"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M12 5L19 12L12 19"
+              stroke="black"
+              strokeWidth="2.35"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+        <span className="text-xl font-[450]">{placeholder}</span>
+      </header>
+      <div className="h-[70px]" />
+    </>
   );
 }
