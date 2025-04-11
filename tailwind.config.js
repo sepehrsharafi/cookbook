842cd2528a9679c8069df7fa27f02e6c5e2ddf2c
sepehrsharafi <sepehr.sharafi123@gmail.com>
2025-04-11 17:29:05 +0330

    FIx: added safelist to tailwind config to ensure ui behavior

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
+  safelist: [
+    "fixed",
+    "bottom-0",
+    "w-full",
+    "bg-slate-900",
+    "text-primary-foreground",
+    "hover:bg-sky-700",
+    "text-lg",
+    "xl:text-2xl",
+    "2xl:text-2xl",
+    "h-[56px]",
+    "rounded-[10px]",
+    "font-medium",
+    "shadow-xs",
+  ],
   theme: {
     extend: {
       fontFamily: {
