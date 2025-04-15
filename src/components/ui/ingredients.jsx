8a4562cfbacd97148c05954bac8071a3910854c1
sepehrsharafi <sepehr.sharafi123@gmail.com>
2025-04-15 23:47:17 +0330

    Task: added a icon to the ingredient item

--- src/components/ui/ingredients.jsx
+++ src/components/ui/ingredients.jsx
@@ -1,11 +1,14 @@
+import { PlusCircle, PlusIcon, PlusSquare } from "lucide-react";
 import { useIngredientContext } from "../../store/ingredient-context";
 
 export default function IngredientItem({ title, toDelete }) {
   return (
-    <div className="flex items-center text-lg font-[450] px-[10px] py-1 bg-[#FDFDFD] border-[1px] border-[#CBD5E1] rounded-md">
-      <span onClick={() => toDelete(title)} className="flex-1">
-        {title}
-      </span>
+    <div
+      onClick={() => toDelete(title)}
+      className="flex items-center text-lg font-[450] px-[10px] py-1 bg-[#FDFDFD] border-[1px] border-[#CBD5E1] rounded-md"
+    >
+      <PlusIcon className="text-red-500 font-[900] stroke-[2] ml-1 -mr-1 transform rotate-45" />
+      {title}
     </div>
   );
 }
